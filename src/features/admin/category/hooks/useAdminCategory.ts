d6f2b7ab83c0101a9759fda4f52ebce5a7c2eff0
Vo Viet Minh Duc <ducvvm4a@gmail.com>
2026-05-08 01:51:37 +0700

    add: integrate api and hooks

--- src/features/admin/category/hooks/useAdminCategory.ts
+++ src/features/admin/category/hooks/useAdminCategory.ts
@@ -1,9 +1,50 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { adminCategoryService } from "../services";
+import type {
+  Category,
+  CreateCategoryRequest,
+  UpdateCategoryRequest,
+} from "../type";
+import { queryClient } from "@/lib/queryClient";
+import { toast } from "sonner";
 
 export const useAdminCategory = () => {
   return useQuery({
     queryKey: ["categories"],
     queryFn: adminCategoryService.getCategories,
+  });
+};
+
+export const useAddCategoryMutation = () => {
+  return useMutation<Category, Error, CreateCategoryRequest>({
+    mutationFn: (payload) => adminCategoryService.addCategory(payload),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["categories"] });
+      toast.success("Category added successfully");
+
+      // đóng modal hoặc về lại trang chính
+    },
+  });
+};
+
+export const useUpdateCategoryMutation = () => {
+  return useMutation<Category, Error, UpdateCategoryRequest>({
+    mutationFn: (payload) => adminCategoryService.updateCategory(payload),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["categories"] });
+      toast.success("Category updated successfully");
+
+      // đóng modal hoặc về lại trang chính
+    },
+  });
+};
+
+export const useDeleteCategoryMutation = () => {
+  return useMutation<void, Error, string>({
+    mutationFn: (id) => adminCategoryService.deleteCategory(id),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["categories"] });
+      toast.success("Category deleted successfully");
+    },
   });
 };
